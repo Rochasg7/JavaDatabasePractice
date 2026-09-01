c0856cc55c1889f8d0471c6a65d448c399ae61f6
Gabriel <gabrielrocha.dev2025@gmail.com>
2026-08-31 21:52:39 -0300

    como funciona o inner join

--- Comandos SQL/Gerencia de estoque.sql
+++ Comandos SQL/Gerencia de estoque.sql
@@ -107,18 +107,32 @@ nome varchar(20)
 insert into categoria
 values (1, 'Mercearia'),
 	     (2, 'Bebidas'),
-	     (4, 'Açougue')
+	     (4, 'Açougue');
 	   
 alter table produto add constraint fk_categoria
-	foreign key (cod_categoria)
-	references categoria (cod)
-	on delete cascade 
-	on update cascade;
+foreign key (cod_categoria)
+references categoria (cod)
+on delete cascade 
+on update cascade;
 
 alter table produto drop constraint fk_categoria
 
 insert into produto
-	values  (1, 'Arroz tipo 1 5kg', 150, 1, 22.50),
-	   		  (2, 'Feijão Carioca 2kg', 120, 1, 6.50),
-	   		  (3, 'Sabão em Pó Omo 1kg', 110, 3, 19.50),
-	   		  (4, 'Pepsi Black 2l', 340, 2, 5.99);
+values  (1, 'Arroz tipo 1 5kg', 150, 1, 22.50),
+	   		(2, 'Feijão Carioca 2kg', 120, 1, 6.50),
+	   		(3, 'Sabão em Pó Omo 1kg', 110, 3, 19.50),
+	   		(4, 'Pepsi Black 2l', 340, 2, 5.99);
+
+-- INNER JOIN
+
+select * from produto
+
+select * from categoria
+
+select p.nome as produto,
+  c.nome as categoria
+from produto as p
+-- considera registros que existem em ambas as tabelas
+inner join categoria as c
+-- chave do relacionamento (pk e fk, normalmente)
+on p.cod_categoria = c.cod
