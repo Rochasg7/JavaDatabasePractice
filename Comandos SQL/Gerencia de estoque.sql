6bebabee32137e31ca783dea1c0bd3add2274005
Gabriel <gabrielrocha.dev2025@gmail.com>
2026-08-31 21:46:49 -0300

    inner join

--- Comandos SQL/Gerencia de estoque.sql
+++ Comandos SQL/Gerencia de estoque.sql
@@ -90,3 +90,35 @@ min(qtde) as "Menor quantidade",
 max(qtde) as "Maior quantidade"
 from estoque
 group by categoria
+
+create table produto (
+cod int PRIMARY KEY,
+nome varchar(50),
+qtde int,
+cod_categoria int,
+vlr_unitario numeric(12,2)
+);
+
+create table categoria (
+cod int primary key,
+nome varchar(20)
+);
+
+insert into categoria
+values (1, 'Mercearia'),
+	     (2, 'Bebidas'),
+	     (4, 'Açougue')
+	   
+alter table produto add constraint fk_categoria
+	foreign key (cod_categoria)
+	references categoria (cod)
+	on delete cascade 
+	on update cascade;
+
+alter table produto drop constraint fk_categoria
+
+insert into produto
+	values  (1, 'Arroz tipo 1 5kg', 150, 1, 22.50),
+	   		  (2, 'Feijão Carioca 2kg', 120, 1, 6.50),
+	   		  (3, 'Sabão em Pó Omo 1kg', 110, 3, 19.50),
+	   		  (4, 'Pepsi Black 2l', 340, 2, 5.99);
